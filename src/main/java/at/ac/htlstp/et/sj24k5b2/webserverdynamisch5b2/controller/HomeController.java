5b57c65b8aa6c751fdbbe91ecbf891aaaef1025e
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2024-11-11 16:25:01 +0100

    Form

--- src/main/java/at/ac/htlstp/et/sj24k5b2/webserverdynamisch5b2/controller/HomeController.java
+++ src/main/java/at/ac/htlstp/et/sj24k5b2/webserverdynamisch5b2/controller/HomeController.java
@@ -4,13 +4,20 @@ import at.ac.htlstp.et.sj24k5b2.webserverdynamisch5b2.model.IndexFormDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String root(Model model) { return abc(model); }
+
+    @GetMapping("/index.html")
+    public String index(Model model) { return abc(model); }
+
     @GetMapping("/abc")
-    public String index(Model model) {
+    public String abc(Model model) {
         IndexFormDto indexFormDto = new IndexFormDto("WERT");
         model.addAttribute("msg", "Hallo 5B2!");
         model.addAttribute("indexFormDto", indexFormDto);
@@ -18,7 +25,10 @@ public class HomeController {
     }
 
     @PostMapping("/res")
-    public String index(Model model, IndexFormDto indexFormDto) {
+    public String index(Model model,  @ModelAttribute IndexFormDto indexFormDto) {
+
+        model.addAttribute("msg", "Antwort 5B2!");
+        System.out.println("Eingegeben wurde: "+indexFormDto.getFeld());
         return "index";
     }
 
